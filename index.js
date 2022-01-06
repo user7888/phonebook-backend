641a89e28ad27eabe01eb573a42ab8ce9c907127
user7888 <junnoelias@gmail.com>
2022-01-06 18:29:52 +0200

    installed cors-middleware

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 morgan.token('type', function (req, res) { return JSON.stringify(req.body) })
 const app = express()
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
+app.use(cors())
 
 let persons = [
         {
