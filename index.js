596fc1253866142c9271a59de86a802db4b4e4dd
user7888 <junnoelias@gmail.com>
2022-01-07 13:44:53 +0200

    initial commit

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ const generateId = () => {
     return newId
   }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {   
     console.log(`Server running on port ${PORT}`)
   })
