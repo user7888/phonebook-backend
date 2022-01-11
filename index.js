43ac206c27b34d0d125c4f7f0603a3dc97b75faf
user7888 <junnoelias@gmail.com>
2022-01-11 21:18:50 +0200

    heroku test

--- index.js
+++ index.js
@@ -258,7 +258,7 @@ const generateId = () => {
     return newId
   }
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {   
     console.log(`Server running on port ${PORT}`)
   })
